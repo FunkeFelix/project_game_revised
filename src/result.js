9c3d8bf978d7da3339428c0266e4c4ea399cb201
FunkeFelix <felixfunke456@gmail.com>
2020-11-05 17:32:58 +0100

    changing time setup

--- src/result.js
+++ src/result.js
@@ -14,7 +14,7 @@ function punshLineGen(shots) {
     punshLine.innerText = "You might want to call an ambulance!";
   } else if (shots < 5 && shots > 3) {
     punshLine.innerText = "You seem to be thirsty!";
-  } else if (shots < 3 && shots > 1) {
+  } else if (shots < 3 && shots >= 1) {
     punshLine.innerText = "You are no Fun!";
   } else {
     punshLine.innerText = "You boring Fuck!";
